8b8784045e660699b58a5f2d5173679d8905d020
atrox3d <atrox3d@gmail.com>
2022-09-07 17:21:27 +0200

    123. The Empty Classes

---
+++ temperature.py
@@ -0,0 +1,10 @@
+class Temperature:
+    """
+    timeanddate.com/weather
+    """
+    def __init__(self, country, city):
+        self.country = country
+        self.city = city
+
+    def get(self):
+        pass
